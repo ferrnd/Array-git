350a8d2a728a168eb07973ea3482c8fc3f7cab2a
Fernando Santos <sombarepam@gmail.com>
2025-04-25 10:19:05 -0300

    Removendo a última coisa que você comprou

---
+++ exercicio5.js
@@ -0,0 +1,5 @@
+let food = ['Melancia' , 'Salmão grelhado' , 'Macarrão'];
+console.log(food);//Antes de remover
+console.log('---------------------------------------------');
+food.pop(food)
+console.log(food)//Depois de remover
