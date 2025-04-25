7e5fe144b3b8c8c28605300b07016e05526f28d5
Fernando Santos <sombarepam@gmail.com>
2025-04-25 09:49:52 -0300

    Corrigindo o nome errado

---
+++ exercicio3.js
@@ -0,0 +1,5 @@
+let Bestfriends = ['Luiz felipe' , 'Lucas Tavares Bento' , 'Yasmin amorzao']
+console.log(Bestfriends)//Errado
+console.log('-------------------------------------------------------------')
+Bestfriends[2] = 'Yasmin crisóstomo'
+console.log(Bestfriends)//Corrigido
