0db5b2622618aa8c4140836109e871e403ca0b05
Fernando Santos <sombarepam@gmail.com>
2025-04-25 11:35:24 -0300

    Ranking Interativo

---
+++ exercicio10.js
@@ -0,0 +1,8 @@
+let jogos = ['Life Is Strange 2', 'Elden Ring', 'Stardew Valley']
+let jogofavorito = 'Elden Ring'
+
+for (let i = 0; i < jogos.length; i++) {
+    if (jogos[i] === jogofavorito) {
+        console.log('Seu jogo favorito está no ranking!')
+    } 
+}
