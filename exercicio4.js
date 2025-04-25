542c0c4b77647db018442a772c760c969919cf59
Fernando Santos <sombarepam@gmail.com>
2025-04-25 09:57:00 -0300

    Adicionando um novo item com .push()

---
+++ exercicio4.js
@@ -0,0 +1,5 @@
+let food = ['Melancia' , 'Salmão grelhado' , 'Macarrão'];
+console.log(food);//Antes de adicionar
+console.log('---------------------------------------------');
+food.push('Água de coco');
+console.log(food)//Depois de adicionar
