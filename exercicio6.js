bb4812459b2059ae169341665624f3eee438852b
Fernando Santos <sombarepam@gmail.com>
2025-04-25 10:29:51 -0300

    Mostrar os itens invertidos

---
+++ exercicio6.js
@@ -0,0 +1,5 @@
+let filmes = ['Django livre', 'Homem-aranha através do aranhaverso', 'Gato de Botas 2'];
+for (let i = filmes.length - 1; i >= - 0; i--); //Ao contrário
+{
+    console.log(filmes[i]);
+}
